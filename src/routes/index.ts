c29e40e35e2714df6a1ee1f8e795306d4877f933
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-10 18:00:13 -0500

    Added test route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,10 @@ import Geolocation from "./Geolocation";
 const router = express.Router();
 
 export default (): express.Router => {
-    
+    router.get('/', (req, res) => {
+        res.send('Hello World');
+    });
+
     Geolocation(router);
     
     return router;
